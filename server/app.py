c31855e9d4e98acfc615c0139c238f818b92f6d3
Noah chirchir <chirchirnoah37@gmail.com>
2024-08-21 19:48:12 +0300

    Initial commit

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 import logging
 from flask import Flask, request, jsonify
-from server.models import db, Booking, Itinerary, TravelJournal, Activity, User, Image, Like, Comment
+from models import db, Booking, Itinerary, TravelJournal, Activity, User, Image, Like, Comment
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager, create_access_token, get_jwt_identity, jwt_required, get_jwt
